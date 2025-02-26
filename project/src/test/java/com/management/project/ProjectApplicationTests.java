270890853703871f651852f909593c9f465e3ff3
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-02-26 13:24:45 -0300

    Creating initial system structure

---
+++ project/src/test/java/com/management/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.management.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
