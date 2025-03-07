31de86f82f6f4db38223b20ca2e9c8492027b025
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-03-07 15:23:55 -0300

    Adding validation in ProjectCreateDTO

--- project/src/main/java/com/management/project/data/dto/project/ProjectCreateDTO.java
+++ project/src/main/java/com/management/project/data/dto/project/ProjectCreateDTO.java
@@ -2,7 +2,10 @@ package com.management.project.data.dto.project;
 
 import com.management.project.model.enums.StatusEnum;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 public record ProjectCreateDTO(
-        String name,
+        @NotBlank @Size(min = 3, max = 100) String name,
         StatusEnum Status
 ) {}
