8a8ff7101ee645eabf38305b9ec429c8335a6532
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-03-11 12:48:00 -0300

    Adding hateoas in the ProjectResponseDTO

--- project/src/main/java/com/management/project/data/dto/project/ProjectResponseDTO.java
+++ project/src/main/java/com/management/project/data/dto/project/ProjectResponseDTO.java
@@ -1,14 +1,77 @@
 package com.management.project.data.dto.project;
 
 import com.management.project.model.enums.StatusEnum;
+import org.springframework.hateoas.RepresentationModel;
+import org.yaml.snakeyaml.representer.Represent;
 
+import java.io.Serial;
+import java.io.Serializable;
 import java.time.Instant;
+import java.util.Objects;
 
-public record ProjectResponseDTO(
-        Long id,
-        String name,
-        StatusEnum status,
-        Instant createdAt,
-        Instant updatedAt
-) {
+public class ProjectResponseDTO extends RepresentationModel<ProjectResponseDTO> implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String name;
+    private StatusEnum status;
+    private Instant createdAt;
+    private Instant updatedAt;
+
+    public ProjectResponseDTO() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public StatusEnum getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusEnum status) {
+        this.status = status;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Instant updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        ProjectResponseDTO that = (ProjectResponseDTO) o;
+        return Objects.equals(id, that.id) && Objects.equals(name, that.name) && status == that.status && Objects.equals(createdAt, that.createdAt) && Objects.equals(updatedAt, that.updatedAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, status, createdAt, updatedAt);
+    }
 }
