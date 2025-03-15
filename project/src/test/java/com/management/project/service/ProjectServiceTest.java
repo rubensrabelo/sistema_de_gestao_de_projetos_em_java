0134c595c015d4201d420767e303218ab64ca481
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-03-15 11:32:06 -0300

    Continuing test with project service

--- project/src/test/java/com/management/project/service/ProjectServiceTest.java
+++ project/src/test/java/com/management/project/service/ProjectServiceTest.java
@@ -1,14 +1,38 @@
 package com.management.project.service;
 
+import com.management.project.mocks.MockProject;
+import com.management.project.model.Project;
+import com.management.project.repository.ProjectRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ExtendWith(MockitoExtension.class)
 class ProjectServiceTest {
 
+    MockProject input;
+
+    @InjectMocks
+    private ProjectService service;
+
+    @Mock
+    private ProjectRepository repository;
+
     @BeforeEach
-    void setUp() {
+    void setUp() throws Exception {
+        input = new MockProject();
+        MockitoAnnotations.openMocks(this);
     }
 
     @Test
@@ -17,6 +41,13 @@ class ProjectServiceTest {
 
     @Test
     void findById() {
+        Project project = input.mockEntity(1);
+
+        when(repository.findById(1L)).thenReturn(Optional.of(project));
+
+        var result = service.findById(1L);
+
+        assertNotNull(result);
     }
 
     @Test
