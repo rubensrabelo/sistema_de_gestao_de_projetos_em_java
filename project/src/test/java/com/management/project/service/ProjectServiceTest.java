612cf7c3a5dbe71c705059df275e8d49060f5775
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-03-15 10:34:52 -0300

    Starting testing with project service

---
+++ project/src/test/java/com/management/project/service/ProjectServiceTest.java
@@ -0,0 +1,33 @@
+package com.management.project.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProjectServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+}
