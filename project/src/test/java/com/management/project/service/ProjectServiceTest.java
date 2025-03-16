924065d597eb918b0bcf61847686635673202505
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-03-16 07:53:37 -0300

    Continuing project service test

--- project/src/test/java/com/management/project/service/ProjectServiceTest.java
+++ project/src/test/java/com/management/project/service/ProjectServiceTest.java
@@ -1,7 +1,10 @@
 package com.management.project.service;
 
+import com.management.project.data.dto.project.ProjectCreateDTO;
+import com.management.project.data.dto.project.ProjectResponseDTO;
 import com.management.project.mocks.MockProject;
 import com.management.project.model.Project;
+import com.management.project.model.enums.StatusEnum;
 import com.management.project.repository.ProjectRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,7 +14,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
 
+import java.time.Instant;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -29,6 +34,9 @@ class ProjectServiceTest {
     @Mock
     private ProjectRepository repository;
 
+    @Mock
+    private ModelMapper modelMapper;
+
     @BeforeEach
     void setUp() throws Exception {
         input = new MockProject();
@@ -42,16 +50,80 @@ class ProjectServiceTest {
     @Test
     void findById() {
         Project project = input.mockEntity(1);
+        ProjectResponseDTO projectDTO = new ProjectResponseDTO();
+
+        projectDTO.setId(project.getId());
+        projectDTO.setName(project.getName());
+        projectDTO.setStatus(project.getStatus());
+        projectDTO.setCreatedAt(project.getCreatedAt());
+        projectDTO.setUpdatedAt(project.getUpdatedAt());
 
         when(repository.findById(1L)).thenReturn(Optional.of(project));
+        when(modelMapper.map(project, ProjectResponseDTO.class)).thenReturn(projectDTO);
 
         var result = service.findById(1L);
 
         assertNotNull(result);
+        assertNotNull(result.getId());
+        assertNotNull(result.getLinks());
+
+        assertNotNull(result.getLinks().stream()
+                .anyMatch(link -> link.getRel().value().equals("self")
+                        && link.getHref().endsWith("/v1/projects/" + project.getId())
+                        && link.getType().equals("GET")
+                )
+        );
+
+        assertNotNull(result.getLinks().stream()
+                .anyMatch(link -> link.getRel().value().equals("findAll")
+                        && link.getHref().endsWith("/v1/projects/")
+                        && link.getType().equals("GET")
+                )
+        );
+
+        assertNotNull(result.getLinks().stream()
+                .anyMatch(link -> link.getRel().value().equals("create")
+                        && link.getHref().endsWith("/v1/projects/")
+                        && link.getType().equals("POST")
+                )
+        );
+
+        assertNotNull(result.getLinks().stream()
+                .anyMatch(link -> link.getRel().value().equals("update")
+                        && link.getHref().endsWith("/v1/projects/" + project.getId())
+                        && link.getType().equals("PUT")
+                )
+        );
+
+        assertNotNull(result.getLinks().stream()
+                .anyMatch(link -> link.getRel().value().equals("delete")
+                        && link.getHref().endsWith("/v1/projects/" + project.getId())
+                        && link.getType().equals("DELETE")
+                )
+        );
+
+        assertEquals("Project 1", result.getName());
+        assertEquals(StatusEnum.NOT_DONE, result.getStatus());
+        assertNotNull(result.getCreatedAt());
+        assertNotNull(result.getUpdatedAt());
     }
 
     @Test
     void create() {
+        ProjectResponseDTO dto = input.mockDTO(1);
+        Project persisted = new Project();
+
+        persisted.setId(dto.getId());
+        persisted.setName(dto.getName());
+        persisted.setStatus(dto.getStatus());
+        persisted.setCreatedAt(dto.getCreatedAt());
+        persisted.setUpdatedAt(dto.getUpdatedAt());
+
+        ProjectCreateDTO dtoCreate = new ProjectCreateDTO(dto.getName(), dto.getStatus());
+        Project project = new Project(null, dtoCreate.getName(), dtoCreate.getStatus(), null, null);
+
+
+        when(modelMapper.map(dtoCreate, Project.class)).thenReturn(project);
     }
 
     @Test
