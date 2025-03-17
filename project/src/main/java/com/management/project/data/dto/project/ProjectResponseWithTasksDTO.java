4a4925ef30619fef56aeabb6f088ff7bf2609f29
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-03-17 12:22:28 -0300

    Adding ProjectResponseWithTasksDTO and fixing ProjectResponseDTO

--- project/src/main/java/com/management/project/data/dto/project/ProjectResponseWithTasksDTO.java
+++ project/src/main/java/com/management/project/data/dto/project/ProjectResponseWithTasksDTO.java
@@ -3,7 +3,6 @@ package com.management.project.data.dto.project;
 import com.management.project.data.dto.task.TaskResponseDTO;
 import com.management.project.model.enums.StatusEnum;
 import org.springframework.hateoas.RepresentationModel;
-import org.yaml.snakeyaml.representer.Represent;
 
 import java.io.Serial;
 import java.io.Serializable;
@@ -11,7 +10,7 @@ import java.time.Instant;
 import java.util.List;
 import java.util.Objects;
 
-public class ProjectResponseDTO extends RepresentationModel<ProjectResponseDTO> implements Serializable {
+public class ProjectResponseWithTasksDTO extends RepresentationModel<ProjectResponseWithTasksDTO> implements Serializable {
 
     @Serial
     private static final long serialVersionUID = 1L;
@@ -22,7 +21,9 @@ public class ProjectResponseDTO extends RepresentationModel<ProjectResponseDTO> 
     private Instant createdAt;
     private Instant updatedAt;
 
-    public ProjectResponseDTO() {
+    private List<TaskResponseDTO> tasks;
+
+    public ProjectResponseWithTasksDTO() {
     }
 
     public Long getId() {
@@ -65,16 +66,24 @@ public class ProjectResponseDTO extends RepresentationModel<ProjectResponseDTO> 
         this.updatedAt = updatedAt;
     }
 
+    public List<TaskResponseDTO> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<TaskResponseDTO> tasks) {
+        this.tasks = tasks;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         if (!super.equals(o)) return false;
-        ProjectResponseDTO that = (ProjectResponseDTO) o;
-        return Objects.equals(id, that.id) && Objects.equals(name, that.name) && status == that.status && Objects.equals(createdAt, that.createdAt) && Objects.equals(updatedAt, that.updatedAt);
+        ProjectResponseWithTasksDTO that = (ProjectResponseWithTasksDTO) o;
+        return Objects.equals(id, that.id) && Objects.equals(name, that.name) && status == that.status && Objects.equals(createdAt, that.createdAt) && Objects.equals(updatedAt, that.updatedAt) && Objects.equals(tasks, that.tasks);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(super.hashCode(), id, name, status, createdAt, updatedAt);
+        return Objects.hash(super.hashCode(), id, name, status, createdAt, updatedAt, tasks);
     }
 }
