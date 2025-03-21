4c92026ef287a5b446c5f95da5017c11906c0f89
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-03-21 08:17:50 -0300

    Creating CollaboratorCreateDTO

---
+++ project/src/main/java/com/management/project/data/dto/collaborator/CollaboratorCreateDTO.java
@@ -0,0 +1,56 @@
+package com.management.project.data.dto.collaborator;
+
+import com.management.project.model.enums.FunctionEnum;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import org.springframework.hateoas.RepresentationModel;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+public class CollaboratorCreateDTO {
+
+    @NotBlank
+    @Size(min = 3, max = 100)
+    private String name;
+
+    @NotBlank
+    @Email
+    private String email;
+    private FunctionEnum function;
+
+    public CollaboratorCreateDTO() {}
+
+    public CollaboratorCreateDTO(String name, String email, FunctionEnum function) {
+        this.name = name;
+        this.email = email;
+        this.function = function;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public FunctionEnum getFunction() {
+        return function;
+    }
+
+    public void setFunction(FunctionEnum function) {
+        this.function = function;
+    }
+}
