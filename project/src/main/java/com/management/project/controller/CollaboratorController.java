b1e84d82cb61773e37696afa48191178d63ab7eb
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-03-21 08:39:48 -0300

    Starting CollaboratorService and CollaboratorController

---
+++ project/src/main/java/com/management/project/controller/CollaboratorController.java
@@ -0,0 +1,78 @@
+package com.management.project.controller;
+
+import com.management.project.controller.docs.ProjectControllerDocs;
+import com.management.project.data.dto.project.ProjectCreateDTO;
+import com.management.project.data.dto.project.ProjectResponseDTO;
+import com.management.project.data.dto.project.ProjectUpdateDTO;
+import com.management.project.service.ProjectService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.PagedModel;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/v1/projects")
+@Tag(name = "Projects", description = "Endpoints for Managing Projects")
+public class CollaboratorController implements ProjectControllerDocs {
+
+    @Autowired
+    private ProjectService service;
+
+    @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE})
+    @Override
+    public ResponseEntity<PagedModel<EntityModel<ProjectResponseDTO>>> findAll(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "size", defaultValue = "10") Integer size,
+            @RequestParam(value = "direction", defaultValue = "asc") String direction
+    ) {
+        var sortDirection = direction.equalsIgnoreCase("asc") ? Direction.ASC : Direction.DESC;
+        Pageable pageable = PageRequest.of(page, size, Sort.by(sortDirection, "name"));
+        return ResponseEntity.ok().body(service.findAll(pageable));
+    }
+
+    @GetMapping(value = "/{id}", produces = {MediaType.APPLICATION_JSON_VALUE})
+    @Override
+    public ResponseEntity<ProjectResponseDTO> findById(@PathVariable Long id) {
+        return ResponseEntity.ok().body(service.findById(id));
+    }
+
+    @PostMapping(
+            produces = {MediaType.APPLICATION_JSON_VALUE},
+            consumes = {MediaType.APPLICATION_JSON_VALUE}
+    )
+    @Override
+    public ResponseEntity<ProjectResponseDTO> create(@RequestBody ProjectCreateDTO dto) {
+        ProjectResponseDTO response = service.create(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(response.getId()).toUri();
+        return ResponseEntity.created(uri).body(response);
+    }
+
+    @PutMapping(
+            value = "/{id}",
+            produces = {MediaType.APPLICATION_JSON_VALUE},
+            consumes = {MediaType.APPLICATION_JSON_VALUE}
+    )
+    @Override
+    public ResponseEntity<ProjectResponseDTO> update(@PathVariable Long id, ProjectUpdateDTO dto) {
+        ProjectResponseDTO response = service.update(id, dto);
+        return ResponseEntity.ok().body(response);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    @Override
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ project/src/main/java/com/management/project/service/CollaboratorService.java
@@ -0,0 +1,141 @@
+package com.management.project.service;
+
+import com.management.project.controller.CollaboratorController;
+import com.management.project.controller.ProjectController;
+import com.management.project.data.dto.collaborator.CollaboratorCreateDTO;
+import com.management.project.data.dto.collaborator.CollaboratorResponseDTO;
+import com.management.project.data.dto.collaborator.CollaboratorUpdateDTO;
+import com.management.project.data.dto.project.ProjectCreateDTO;
+import com.management.project.data.dto.project.ProjectResponseDTO;
+import com.management.project.data.dto.project.ProjectUpdateDTO;
+import com.management.project.model.Collaborator;
+import com.management.project.model.Project;
+import com.management.project.repository.CollaboratorRepository;
+import com.management.project.repository.ProjectRepository;
+import com.management.project.service.exceptions.*;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.PagedModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.stereotype.Service;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+
+@Service
+public class CollaboratorService {
+
+    @Autowired
+    private CollaboratorRepository repository;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Autowired
+    PagedResourcesAssembler<CollaboratorResponseDTO> assembler;
+
+    public PagedModel<EntityModel<CollaboratorResponseDTO>> findAll(Pageable pageable) {
+        var dtoResponse = repository.findAll(pageable)
+                .map(coll ->{
+                    var dto = modelMapper.map(coll, CollaboratorResponseDTO.class);
+                    addHateoasLinks(dto);
+                    return dto;
+                });
+        Link findAllLinks = WebMvcLinkBuilder.linkTo(
+                WebMvcLinkBuilder.methodOn(CollaboratorController.class)
+                        .findAll(
+                                pageable.getPageNumber(),
+                                pageable.getPageSize(),
+                                String.valueOf(pageable.getSort())
+                        )
+        ).withSelfRel();
+        return assembler.toModel(dtoResponse, findAllLinks);
+    }
+
+    public CollaboratorResponseDTO findById(Long id) {
+        Collaborator entity = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Collaborator not found"));
+        CollaboratorResponseDTO dtoResponse = modelMapper.map(entity, CollaboratorResponseDTO.class);
+        addHateoasLinks(dtoResponse);
+        return dtoResponse;
+    }
+
+    public CollaboratorResponseDTO create(CollaboratorCreateDTO dto) {
+        if(dto == null)
+            throw new RequiredObjectIsNullException();
+
+        if(dto.getName() == null || dto.getName().isEmpty())
+            throw new EmptyNameException("The name cannot be null or blank.");
+
+        if(dto.getName().length() < 3 || dto.getName().length() > 100)
+            throw new InvalidNameSizeException("The name field must be between 3 and 100 characters.");
+
+
+        Collaborator entity = modelMapper.map(dto, Collaborator.class);
+        repository.save(entity);
+        ProjectResponseDTO dtoResponse = modelMapper.map(entity, CollaboratorResponseDTO.class);
+        addHateoasLinks(dtoResponse);
+        return dtoResponse;
+    }
+
+    public CollaboratorResponseDTO update(Long id, CollaboratorUpdateDTO updatedData) {
+        if(updatedData == null)
+            throw new RequiredObjectIsNullException();
+
+        Collaborator entity = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Collaborator not found"));
+
+        updateData(entity, updatedData);
+        repository.save(entity);
+        CollaboratorResponseDTO dtoResponse = modelMapper.map(entity, CollaboratorResponseDTO.class);
+        addHateoasLinks(dtoResponse);
+        return dtoResponse;
+    }
+
+    public void deleteById(Long id) {
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException("Project not found");
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
+    }
+
+    private void updateData(Collaborator entity, CollaboratorUpdateDTO updatedData) {
+        if(updatedData.getName() != null) {
+            if(updatedData.getName().length() < 3 || updatedData.getName().length() > 100)
+                throw new InvalidNameSizeException("The name field must be between 3 and 100 characters.");
+            entity.setName(updatedData.getName());
+        }
+
+        if(updatedData.getEmail() != null) {
+            if(updatedData.getEmail().length() > 100)
+                throw new InvalidNameSizeException("The email field must have a maximum of 150 characters.");
+            entity.setEmail(updatedData.getEmail());
+        }
+
+        if(updatedData.getFunction() != null)
+            entity.setFunction(updatedData.getFunction());
+    }
+
+    private void addHateoasLinks(ProjectResponseDTO dto) {
+        dto.add(linkTo(methodOn(ProjectController.class).findAll(0, 10, "desc")).withRel("findAll").withType("GET"));
+        dto.add(linkTo(methodOn(ProjectController.class).findById(dto.getId())).withSelfRel().withType("GET"));
+
+        ProjectCreateDTO dtoCreated = new ProjectCreateDTO(dto.getName(), dto.getStatus());
+        dto.add(linkTo(methodOn(ProjectController.class).create(dtoCreated)).withRel("create").withType("POST"));
+
+        ProjectUpdateDTO dtoUpdated = new ProjectUpdateDTO(dto.getName(), dto.getStatus());
+        dto.add(linkTo(methodOn(ProjectController.class).update(dto.getId(), dtoUpdated)).withRel("update").withType("PUT"));
+
+        dto.add(linkTo(methodOn(ProjectController.class).delete(dto.getId())).withRel("delete").withType("DELETE"));
+    }
+}
