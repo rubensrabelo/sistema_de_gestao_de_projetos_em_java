0704f55fa5dbeb1230af5dd772f8c2045a9f264a
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-03-23 14:32:49 -0300

    Starting CollaboratorService test

---
+++ project/src/test/java/com/management/project/unittest/service/CollaboratorServiceTest.java
@@ -0,0 +1,31 @@
+package com.management.project.unittest.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CollaboratorServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+}
