d516d66ee710662bae6818c40747cd5d29f6000c
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-03-28 08:01:56 -0300

    Continuing Tast Service Test with create, update and assignCollaboratorToTask methods

--- project/src/test/java/com/management/project/unittest/service/TaskServiceTest.java
+++ project/src/test/java/com/management/project/unittest/service/TaskServiceTest.java
@@ -2,13 +2,16 @@ package com.management.project.unittest.service;
 
 import com.management.project.data.dto.collaborator.CollaboratorCreateDTO;
 import com.management.project.data.dto.collaborator.CollaboratorResponseDTO;
+import com.management.project.data.dto.collaborator_task.CollaboratorTaskDTO;
 import com.management.project.data.dto.task.TaskCreateDTO;
 import com.management.project.data.dto.task.TaskResponseDTO;
+import com.management.project.data.dto.task.TaskUpdateDTO;
 import com.management.project.model.Collaborator;
 import com.management.project.model.Project;
 import com.management.project.model.Task;
 import com.management.project.model.enums.FunctionEnum;
 import com.management.project.model.enums.StatusEnum;
+import com.management.project.repository.CollaboratorRepository;
 import com.management.project.repository.ProjectRepository;
 import com.management.project.repository.TaskRepository;
 import com.management.project.service.TaskService;
@@ -24,10 +27,14 @@ import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
 
+import java.time.Instant;
+import java.util.Map;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
+
+// Falta fazer os testes de execeções
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @ExtendWith(MockitoExtension.class)
@@ -43,6 +50,9 @@ class TaskServiceTest {
 
     @Mock
     private ProjectRepository projectRepository;
+
+    @Mock
+    private CollaboratorRepository collaboratorRepository;
 
     @Mock
     private ModelMapper modelMapper;
@@ -97,7 +107,6 @@ class TaskServiceTest {
         verifyNoInteractions(modelMapper);
     }
 
-    // Continuar nesse teste
     @Test
     void create() {
         TaskCreateDTO dtoCreate = new TaskCreateDTO("New Task", StatusEnum.DOING, 1L);
@@ -106,24 +115,87 @@ class TaskServiceTest {
         dtoResponse.setId(1L);
         dtoResponse.setName("New Task");
         dtoResponse.setStatus(StatusEnum.DOING);
+        dtoResponse.setCreatedAt(Instant.now());
+        dtoResponse.setUpdatedAt(Instant.now());
 
 
         when(projectRepository.findById(1L)).thenReturn(Optional.of(new Project()));
         when(taskRepository.save(any(Task.class))).thenReturn(entity);
         when(modelMapper.map(entity, TaskResponseDTO.class)).thenReturn(dtoResponse);
 
-        TaskResponseDTO response = taskService.create(dtoCreate);
-        assertNotNull(response);
-        assertEquals("New Task", response.getName());
-        assertTrue(response.getLinks().hasLink("self"));
+        TaskResponseDTO result = taskService.create(dtoCreate);
+
+        assertNotNull(result);
+        assertNotNull(result.getId());
+        assertNotNull(result.getLinks());
+
+        assertEquals("New Task", result.getName());
+        assertEquals(StatusEnum.DOING, result.getStatus());
+        assertNotNull(result.getCreatedAt());
+        assertNotNull(result.getUpdatedAt());
+
+        assertLinkExists(result, "self", "/v1/tasks/" + dtoResponse.getId(), "GET");
+        assertLinkExists(result, "create", "/v1/tasks", "POST");
+        assertLinkExists(result, "update", "/v1/tasks/" + dtoResponse.getId(), "PUT");
+
+        verify(projectRepository, times(1)).findById(1L);
+        verify(taskRepository, times(1)).save(entity);
+        verify(modelMapper, times(1)).map(entity, TaskResponseDTO.class);
     }
 
     @Test
     void assignCollaboratorToTask() {
+        Task entityTask = input.mockEntity(1);
+        Collaborator entityCollaborator = new Collaborator(
+                "Collaborator 1",
+                "test@test.com",
+                FunctionEnum.DEVELOPER
+        );
+        entityCollaborator.setId(1L);
+        CollaboratorTaskDTO dtoResponse = new CollaboratorTaskDTO(1L, 1L);
+
+        when(taskRepository.findById(1L)).thenReturn(Optional.of(entityTask));
+        when(collaboratorRepository.findById(1L)).thenReturn(Optional.of(entityCollaborator));
+
+        Map<String, String> response = taskService.assignCollaboratorToTask(dtoResponse);
+
+        assertEquals("Collaborator: Collaborator 1 assigned to Task: Task 1", response.get("message"));
+        assertTrue(entityTask.getCollaborators().contains(entityCollaborator));
+
+        verify(taskRepository).save(entityTask);
+        verify(taskRepository, times(1)).findById(1L);
+        verify(collaboratorRepository, times(1)).findById(1L);
     }
 
     @Test
     void update() {
+        TaskUpdateDTO updateDTO = new TaskUpdateDTO("Updated Task", StatusEnum.NOT_DONE);
+        Task entity = input.mockEntity(1);
+        TaskResponseDTO dtoResponse = input.mockDTO(1);
+        entity.setName("Updated Task");
+
+        when(taskRepository.findById(1L)).thenReturn(Optional.of(entity));
+        when(taskRepository.save(any(Task.class))).thenReturn(entity);
+        when(modelMapper.map(entity, TaskResponseDTO.class)).thenReturn(dtoResponse);
+
+        TaskResponseDTO result = taskService.update(1L, updateDTO);
+
+        assertNotNull(result);
+        assertNotNull(result.getId());
+        assertNotNull(result.getLinks());
+
+        assertEquals("TaskDTO 1", result.getName());
+        assertEquals(StatusEnum.NOT_DONE, result.getStatus());
+        assertNotNull(result.getCreatedAt());
+        assertNotNull(result.getUpdatedAt());
+
+        assertLinkExists(result, "self", "/v1/tasks/" + dtoResponse.getId(), "GET");
+        assertLinkExists(result, "create", "/v1/tasks", "POST");
+        assertLinkExists(result, "update", "/v1/tasks/" + dtoResponse.getId(), "PUT");
+
+        verify(taskRepository, times(1)).findById(1L);
+        verify(taskRepository, times(1)).save(entity);
+        verify(modelMapper, times(1)).map(entity, TaskResponseDTO.class);
     }
 
     private void assertLinkExists(TaskResponseDTO dto, String rel, String href, String type) {
